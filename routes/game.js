b04745a45c5a64a55216b8fd75b3d8e714941807
reidnaa <reidnaaykens@gmail.com>
2017-12-19 00:16:34 +0000

    Routes set up

---
+++ routes/game.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+//add back knex when database is set up properly **************
+module.exports = () => {
+
+  // Gets the current signed in user.
+  router.get("/", (req, res) => {
+  res.render("game");
+
+  });
+ // Gets the current signed in user.
+  router.post("/", (req, res) => {
+    res.render("game");
+
+  });
+
+
+  return router;
+};
+
+
