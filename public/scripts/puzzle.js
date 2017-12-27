40454eff906b1b4a5409769dc6293f32a4e50440
chrynuik <catherine.hrynuik@gmail.com>
2017-12-27 19:16:54 +0000

    new puzzle file for refactoring

---
+++ public/scripts/puzzle.js
@@ -0,0 +1,75 @@
+// Logic for the Puzzle
+
+var grid = document.querySelector('.grid');
+var pckry = new Packery( grid, {
+  columnWidth: '.grid-sizer',
+  itemSelector: '.tile',
+  percentPosition: true,
+  transitionDuration: '0.3s'
+});
+
+pckry.getItemElements().forEach( function( itemElem ) {
+  var draggie = new Draggabilly( itemElem );
+  pckry.bindDraggabillyEvents( draggie );
+});
+
+// map items by their data-tile
+var mappedItems = {};
+
+var dialog = document.querySelector('.dialog');
+
+var orders = [
+  'abcdefghijklm',
+  // 'fmgdbalkjihec', //remove later
+  'ecdibmhfajkgl',
+  'ilckfgdebhjam'
+];
+
+var didWin = true;
+var orderIndex = 0;
+
+pckry.items.forEach( function( item ) {
+  var attr = item.element.getAttribute('data-tile');
+  mappedItems[ attr ] = item;
+});
+
+/*
+ * Shuffles the tiles in the game area
+ */
+function shuffleTiles() {
+  // shuffle items
+  orderIndex++;
+  var order = orders[ orderIndex % 3 ];
+  pckry.items = order.split('').map( function( attr ) {
+    return mappedItems[ attr ];
+  });
+  // stagger transition
+  pckry._resetLayout();
+  pckry.items.forEach( function( item, i ) {
+    setTimeout( function() {
+      pckry.layoutItems( [ item ] );
+    }, i * 34 );
+  });
+}
+
+/*
+ * Loads the dialog box
+ */
+function showDialog() {
+  dialog.classList.remove('is-waiting');
+}
+
+/*
+ * Checks when a div is moved if the puzzle is correctly conpleted
+ * If yes, loads the win function
+ *500 for large screen 375 for small
+ */
+pckry.on( 'dragItemPositioned', function() {
+  var order = pckry.items.map( function( item ) {
+    return item.element.getAttribute('data-tile');
+  }).join('');
+  if ( order === 'fmgdbalkjihce' && ( pckry.maxY === 500 || pckry.maxY === 375 )) {
+    win();
+  }
+});
+
