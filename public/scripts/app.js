06376b8bf5a55429aff4a7d745d1831c01756009
MarkZsombor <markszsombor@gmail.com>
2018-01-03 21:13:13 +0000

    solo games now load an image at random

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -215,6 +215,13 @@ $('.close-dialog-button').on('click', function(){
 
 $('#play-solo').on('click', function() {
   console.log('Started a single player game');
+  currentRoom.pictureId = Math.ceil(Math.random() * 5);
+  console.log('loading picture', currentRoom.pictureId);
+  if ($(window).width() <= 500) {
+    $('.tile').css("background-image", `url('public/images/cat${currentRoom.pictureId}-sm.jpg')`);
+  } else {
+    $('.tile').css("background-image", `url('public/images/cat${currentRoom.pictureId}-lrg.jpg')`);
+  }
   $(".game").css("display", "block");
   $(".non-game").css("display", "none");
   shuffleTiles();
